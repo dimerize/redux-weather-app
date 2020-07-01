5a705204a9d2deda07cf5179819a1fcf42c92f19
Omar Khan <ok2282@columbia.edu>
2020-07-01 14:26:21 -0500

    Redux support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider }from 'react-redux';
+
+let store = createStore();
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
