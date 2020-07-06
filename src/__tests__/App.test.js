f1a2ded46b7d9732ca5c267cda8bd6b20de5e1f4
Omar Khan <ok2282@columbia.edu>
2020-07-06 12:48:53 -0500

    testing

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -7,5 +7,9 @@ describe('actions', function() {
     it('should have a type of "CHANGE_LOCATION"', function() {
       expect(changeLocation().type).toEqual('CHANGE_LOCATION');
     });
+
+    it('should return an undefined location by default', function() {
+      expect(changeLocation().location).toEqual(undefined);
+    });
   });
 });
