95e0ad0d8d83eab067550d2bbb423982b446e32e
Omar Khan <ok2282@columbia.edu>
2020-07-06 12:38:57 -0500

    Testing

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,8 +2,10 @@ import {
   changeLocation
 } from '../Actions';
    
-  describe('actions', function() {
-    describe('changeLocation', function () {
-   
+describe('actions', function() {
+  describe('changeLocation', function () {
+    it('should have a type of "CHANGE_LOCATION"', function() {
+      expect(changeLocation().type).toEqual('CHANGE_LOCATION');
     });
   });
+});
